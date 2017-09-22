5df338e0042e627029139c7e8515667c6a37340a
Jerry Shi <szy0syz@gmail.com>
2017-09-22 14:41:50 +0800

    updated CH08_06 mixin

---
+++ CH08/06_mixins/src/js/components/mixins.js
@@ -0,0 +1,10 @@
+const MixinLog = {
+  componentDidMount(){
+    console.log('MixinLog componentDidMount...');
+  },
+  log(){
+    console.log('233,MixinLog...');
+  }
+};
+
+export default MixinLog
