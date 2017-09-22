2d554dba4b6955f2e72038f2df83aca4a3551932
Jerry Shi <szy0syz@gmail.com>
2017-09-22 13:10:36 +0800

    finished CH08_04 reusable conponents

---
+++ CH08/03_reusable/src/js/components/bodyIndex.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import BodyChild from './bodyChild';
+
+const defaultProps = {
+  username: 'Defaule-123'
+}
+
+export default class BodyIndex extends React.Component {
+  // 这不是ES6中类的构造函数嘛
+  constructor() {
+    // 调用父类的所有初始化方法
+    super(); //以前学过了
+    this.state = { username: 'Jerry', userage: 18 };
+  }
+
+  changeUserInfo(age) {
+    this.setState({ userage: age });
+  }
+
+  handleChildValueChange(event) {
+    this.setState({ userage: event.target.value });
+  }
+
+  render() {
+    // setTimeout(() => {
+    //   // 更改state时用
+    //   this.setState({ username: 'afei233' });
+    // }, 3000);
+
+    return (
+      <div>
+        <h2>我是页面的主要内容</h2>
+        <p>接收父组件传递的：userid: {this.props.userid}, username: {this.props.username}, userage: {this.props.userage}</p>
+        <p>{this.state.userage}</p>
+        {/* <input type="button" value="submit" onClick={this.changeUserInfo} />  这里修修改函数里的this，不修改this是button对象了*/}
+        <input type="button" value="submit" onClick={this.changeUserInfo.bind(this, 97)} />
+        <BodyChild {...this.props} xid={444} handleChildValueChange={this.handleChildValueChange.bind(this)} />
+      </div>
+    )
+  }
+}
+
+BodyIndex.propTypes = {
+  userid: React.PropTypes.number.isRequired,
+  userage: React.PropTypes.number
+}
+
+BodyIndex.defaultProps = defaultProps;
