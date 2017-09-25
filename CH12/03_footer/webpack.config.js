2cbc0897f758915e98715ff03528eb2ef33926cb
szy0syz <szy0syz@gmail.com>
2017-09-25 10:23:10 +0800

    updated CH12_03&04 PC和移动端页脚组件

---
+++ CH12/03_footer/webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: __dirname + '/src',
+  entry: './js/root.js',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/, // 正则以.js后缀结尾
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: { presets: ['react', 'es2015'] }
+      },
+      //下面是添加的 css 的 loader，也即是 css 模块化的配置方法，大家可以拷贝过去直接使用
+      {
+        test: /\.css?$/, //正则匹配以.css结尾
+        loader: 'style-loader!css-loader'
+        // loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  }
+};
