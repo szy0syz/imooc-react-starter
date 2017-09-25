32c412518fb5008b0c1b261060d033d4a9590623
szy0syz <szy0syz@gmail.com>
2017-09-25 10:11:03 +0800

    updated 完成移动端页头组件

--- CH12/01/src/js/root.js
+++ CH12/01/src/js/root.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-
+import MediaQuery from 'react-responsive';
 import PCIndex from './components/pc_index';
+import MobileIndex from './components/mobile_index';
 
 // import {
 //   BrowserRouter as Router,
@@ -14,7 +15,12 @@ export default class Root extends React.Component {
   render() {
     return (
       <div>
-        <PCIndex></PCIndex>
+        <MediaQuery query="(min-device-width: 1224px)">
+          <PCIndex></PCIndex>
+        </MediaQuery>
+        <MediaQuery query="(max-device-width: 1224px)">
+          <MobileIndex></MobileIndex>
+        </MediaQuery>
       </div>
     );
   };
