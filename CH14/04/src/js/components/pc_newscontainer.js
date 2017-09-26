561f8b138cb753a7a029ec3e0da6113c8c284a7e
szy0syz <szy0syz@gmail.com>
2017-09-26 23:10:44 +0800

    updated 完成pc端newsBlock组件

---
+++ CH14/04/src/js/components/pc_newscontainer.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Row, Col, Tabs, Carousel } from 'antd';
+const TabPane = Tabs.TabPane;
+
+import PCNewsBlock from './pc_news_block';
+
+export default class PCNewsContatiner extends React.Component {
+  render() {
+    const settings = {
+      dots: true,
+      speed: 500,
+      autoplay: true
+    };
+
+    return (
+      <div>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20} className="container">
+            <div className="leftContainer">
+              <div className="carousel">
+                <Carousel {...settings}>
+                  <div><img src="/images/carousel_1.jpg" />></div>
+                  <div><img src="/images/carousel_2.jpg" />></div>
+                  <div><img src="/images/carousel_3.jpg" />></div>
+                  <div><img src="/images/carousel_4.jpg" />></div>
+                </Carousel>
+              </div>
+            </div>
+            <Tabs className="tabs_news">
+              <TabPane tab="头条" key="1">
+                <PCNewsBlock count={12} type="top" width="100%" bordered="false"></PCNewsBlock>
+              </TabPane>
+              <TabPane tab="国内" key="2">
+                <PCNewsBlock count={12} type="guonei" width="100%" bordered="false"></PCNewsBlock>
+              </TabPane>
+              <TabPane tab="国际" key="3">
+                <PCNewsBlock count={12} type="guoji" width="100%" bordered="false"></PCNewsBlock>
+              </TabPane>
+              <TabPane tab="社会" key="4">
+                <PCNewsBlock count={12} type="shehui" width="100%" bordered="false"></PCNewsBlock>
+              </TabPane>
+              <TabPane tab="娱乐" key="5">
+                <PCNewsBlock count={12} type="yule" width="100%" bordered="false"></PCNewsBlock>
+              </TabPane>
+              <TabPane tab="体育" key="6">
+                <PCNewsBlock count={12} type="tiyu" width="100%" bordered="false"></PCNewsBlock>
+              </TabPane>
+              <TabPane tab="科技" key="7">
+                <PCNewsBlock count={12} type="keji" width="100%" bordered="false"></PCNewsBlock>
+              </TabPane>
+            </Tabs>
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+      </div>
+    );
+  }
+}
